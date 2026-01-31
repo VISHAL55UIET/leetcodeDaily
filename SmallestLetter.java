28f1a5b5c0b57c6c51418504700d4451f97b16c2
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-01-31 09:26:38 +0530

    Find Smallest Letter Greater Than Target 31/01/2026

---
+++ SmallestLetter.java
@@ -0,0 +1,71 @@
+import java.util.*;
+/*********************************************************************************************** */
+
+
+
+
+
+   //             T.C=O(n)
+
+
+
+
+
+
+
+// METHOD -1 LENEAR SEARCH
+public class SmallestLetter {
+       public char nextGreatestLetter(char[] letters, char target) {
+        for(char ch : letters){
+            if(ch>target) return ch;
+        }
+        return letters[0];
+    }
+}
+
+
+
+
+
+/************************************************************************************************** */
+
+
+
+// METHOD -2 BINARY SEARCH
+
+
+
+
+
+// T.C=O(nlogn)
+
+
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int lo = 0, hi =  letters.length-1;
+        char ans =  letters[0];
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(letters[mid]>target){
+                ans = letters[mid];
+                hi =  mid-1;
+            } else{
+                lo = mid+1;
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/************************************************************************************************* */
