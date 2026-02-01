f386efef6ca9dad6bdf77361dfba9d7e56947010
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-02-01 10:03:03 +0530

    SecondMethod

--- MinimumCostSubarray.java
+++ MinimumCostSubarray.java
@@ -34,3 +34,20 @@ public int minimumCost(int[] nums) {
 /******************************************************************************************* */
 
 
+/*
+
+ME.T.H.O.D=2
+
+
+
+class Solution {
+    public int minimumCost(int[] nums) {
+        int n =  nums.length;
+        int first =  nums[0];
+        Arrays.sort(nums,1,n);
+        return first+nums[1]+nums[2];
+    }
+}
+
+
+*/
