c2f0d1b8edebc96416d8dbdd4c96c5ac0fa1df2a
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-02-08 10:40:10 +0530

    110. Balanced Binary Tree

---
+++ balancedTree.java
@@ -0,0 +1,50 @@
+import java.util.*;
+/********************************************************************************************88 */
+
+    //T.C = O(N^2)
+
+    // S.C = O(1)
+
+
+
+/************************************************************************************************** */
+
+
+public class balancedTree {
+  
+
+public class TreeNode {
+     int val;
+    TreeNode left;
+      TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+       this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+  }
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = height(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = height(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1)
+            return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+
+/**************************************************************************************************** */
