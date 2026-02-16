85781fc47e4c5caf2ae5b12fd21246e5096b9678
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-02-16 09:24:53 +0530

    190. Reverse Bits

---
+++ reversebit.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/******************************************************************************************* */
+
+
+
+
+
+
+
+
+
+/********************************************************************************************** */
+public class reversebit {
+   public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;          // shift result left
+            result |= (n & 1);     // add last bit of n
+            n >>>= 1;              // unsigned right shift
+        }
+
+        return result;
+    }
+}
