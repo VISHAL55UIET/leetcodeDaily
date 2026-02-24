c80803e48ac44da378713a430bd1bb058e73a776
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-02-24 07:05:24 +0530

    1022. Sum of Root To Leaf Binary Numbers

--- BinaryRootToleafSum.java
+++ BinaryRootToleafSum.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 
 /************************************************************************************************ */
+
 public class BinaryRootToleafSum {
    public class TreeNode {
       int val;
@@ -29,11 +30,11 @@ public class BinaryRootToleafSum {
          public int sumRootToLeaf(TreeNode root) {
         return sumRootToLeaf(root, 0);
     }
-    
+
     public int sumRootToLeaf(TreeNode root, int sum){
         if(root == null) return 0;
         sum = (2 * sum) + root.val;
         if(root.left == null && root.right == null) return sum;
         return sumRootToLeaf(root.left, sum) + sumRootToLeaf(root.right, sum);
-    }
-}
+    }   
+} 
