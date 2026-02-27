f2cd09d81104eba8f70aa581312ab7500a49690e
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-02-27 07:33:01 +0530

    3666. Minimum Operations to Equalize Binary String

---
+++ MinimumOperationEqulizeStringg.java
@@ -0,0 +1,67 @@
+import java.util.*;
+/************************************************************************************************** */
+
+
+
+
+
+
+
+
+
+/************************************************************************************************ */
+public class MinimumOperationEqulizeStringg {
+      public int minOperations(String s, int k) {
+        int zeros = s.chars().map(x->x=='0'?1:0).sum();
+        if((zeros % k) == 0) return zeros / k;
+        int n = s.length();
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(zeros);
+        int res = 1;
+
+        // use bounds for optimization
+        int[][] bounds = new int[2][2];
+        bounds[zeros&1][0] = bounds[zeros&1][1] = zeros;
+        bounds[1-(zeros&1)][0] = Integer.MAX_VALUE;
+        bounds[1-(zeros&1)][1] = Integer.MIN_VALUE;
+
+        while(!q.isEmpty()) {
+            // find min number of zeros and max number of zeros in this round
+            int minv = Integer.MAX_VALUE;
+            int maxv = Integer.MIN_VALUE;
+            for(int len = q.size(); len > 0; len--) {
+                int h = q.poll();
+                int t = n - h;
+                int x = Math.min(h, k);
+                if(t >= k - x) {
+                    int fst = h - x + (k - x);
+                    minv = Math.min(minv, fst);
+                    maxv = Math.max(maxv, fst);
+                }
+                x = Math.min(t, k);
+                if(h >= k - x) {
+                    int snd = h - (k - x) + x;
+                    minv = Math.min(minv, snd);
+                    maxv = Math.max(maxv, snd);
+                }
+            }
+            // possible children are sequence of equal difference 2
+            int ind = minv & 1;
+            int temp = minv;
+            while(temp <= maxv) {
+                if((temp % k) == 0) return res + temp/k;
+                if(temp < bounds[ind][0] || temp > bounds[ind][1]) {
+                    q.add(temp);
+                    temp += 2;
+                }else {
+                    temp = bounds[ind][1] + 2;
+                }
+            }
+            bounds[ind][0] = Math.min(bounds[ind][0], minv);
+            bounds[ind][1] = Math.max(bounds[ind][1], maxv);
+            res++;
+        }
+        return -1;
+    }
+}
