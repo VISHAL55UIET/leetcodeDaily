e9d2ec560951227a2701a63d8b2e8fd467d2b5c7
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-02-28 07:09:25 +0530

    1680. Concatenation of Consecutive Binary Numbers

---
+++ BinarayNumbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+/************************************************************************************************** */
+
+
+
+
+
+
+
+
+/*************************************************************************************************** */
+public class BinarayNumbers {
+     public int concatenatedBinary(int n) {
+        final long modulo = (long) (1e9 + 7);
+        long result = 0;
+        int binaryDigits = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) binaryDigits++;
+            result = ((result << binaryDigits) + i) % modulo;
+        }
+        return (int) result;
+    }
+}
