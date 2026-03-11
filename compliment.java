6c0e6c59a77602088d9fe30a8821f65a315e58db
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-03-11 09:27:43 +0530

    ADD MULTIPLE MTEHOD

--- compliment.java
+++ compliment.java
@@ -24,3 +24,53 @@ public class compliment {
         return num ^ mask;   
     }
 }
+
+
+
+
+
+/* 
+
+//     SECOND METHOD
+
+
+class Solution {
+    public int findComplement(int num) {
+      return ((Integer.highestOneBit(num) << 1) - 1) ^ num;   
+    }
+}
+
+
+// THIRD METHOD
+
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n==0)return 1;
+        int mask = 0;
+        int temp = n;
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+        return n ^ mask;
+    }
+}
+
+// FOURTH METHOD
+class Solution {
+    public int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
+        if (n == 0) {
+            return 1;
+        }
+        while (m != 0) {
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        }
+        int ans = (~n) & mask;
+        return ans;
+    }
+}
+
+*/
