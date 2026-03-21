403d5b6c157792556317a241072b091f9d5f55dd
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-03-21 10:56:22 +0530

    3643. Flip Square Submatrix Vertically

---
+++ VarticalSubMatrix.java
@@ -0,0 +1,32 @@
+import java.util.*;
+/****************************************************************************************************** */
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************************************* */
+public class VarticalSubMatrix {
+    public static int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        int startRow = x;
+        int endRow = x + k - 1;
+        int startCol = y;
+        int endCol = y + k - 1;
+        for (int i = startRow; i <= endRow; i++) {
+            for (int j = startCol; j <= endCol; j++) {
+                // swap(grid[i][j], grid[endRow][j])
+                int temp = grid[i][j];
+                grid[i][j] = grid[endRow][j];
+                grid[endRow][j] = temp;
+            }
+            endRow--;
+        }
+        return grid;
+    }  
+}
